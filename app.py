7e73befbf09e8c50025dc4788672e0decd6d697f
hanseltertius <hansel.tertius@gmail.com>
2025-05-29 19:41:49 +0700

    Implement generate insight feature

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import streamlit as st
+import openai
+
+# region Initialize API key
+openai.api_key = st.secrets.get("OPENAI_API_KEY")
+# endregion
 
 # region Variables
 ALLOWED_FILE_TYPES = ["csv", "xlsx", "xls", "xlsm"]
@@ -24,6 +29,77 @@ def is_valid_uploaded_file(uploaded_file):
     
 def reset_uploaded_file():
     st.session_state.uploaded_file = None
+
+# TODO : create reusable function to display data frame
+def display_dataframe(uploaded_file = None, selected_sheet_name = "", is_excel=True):
+    if is_excel:
+        st.markdown(f"Displayed data from sheet: ```{selected_sheet_name}```")
+        df = pd.read_excel(excel_file, sheet_name=selected_sheet_name)
+        st.dataframe(df)
+        display_tabs(df)
+    else:
+        df = pd.read_csv(uploaded_file)
+        st.dataframe(df)
+        display_tabs(df)
+
+# TODO : create reusable function to display tabs
+def display_tabs(df):
+    tab_summary, tab_insight, tab_chart = st.tabs(["Summary", "Insight", "Chart"])
+
+    with tab_summary:
+        # TODO : maybe using a summary again, inside tab there is another tab
+        # TODO : summarize columns by (select columns)
+        # TODO : do summary of the tabs
+        # st.write(df.describe())
+        # st.write(df.info())
+        st.write("Summary")
+
+        # TODO : maybe we can summarize from 2 columns and some stuff, maybe can select multiple columns to generate summary, ini mesti dapat info2 dari dataframe bgmn
+
+    with tab_insight:
+        user_input = st.text_area("Insight", key="insight_input", placeholder="Write your insight here... (Leading / Trailing whitespaces will be trimmed at the input generation)")
+
+        if st.button("Generate Insight", use_container_width=True):
+            reformatted_user_input = user_input.strip()
+            if reformatted_user_input:
+                with st.spinner("Generating insight..."):
+                    # TODO : getting the dataframe
+                    generated = generate_insight_from_openai(reformatted_user_input, df)
+                    st.write(generated)
+                # TODO : kita bakal generating dl (smua layout di disable (maybe))
+
+                # TODO : response need to generate text to speech (use hugging face models)
+            else:
+                st.warning("Insight input cannot be empty.")
+    with tab_chart:
+        # TODO : tinggal generate the chart based on select box
+        # TODO : setelah pake generate the chart based on select box, tinggal pake button, trus generate popup ny
+        # TODO : generate summary
+        st.write("Chart")
+    # TODO : display tabs (summary, insight and charts)
+
+def generate_insight_from_openai(user_input, df):
+    csv_data = df.to_csv(index=False)
+    prompt = f"""
+    Given the following data table and user question, provide a data insight or analysis.
+    Data Table: 
+    {csv_data}
+    User Question: 
+    {user_input}
+    Can you generate the the insight based on "User Question".
+    """
+
+    response = openai.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # TODO : it potentially get out of index, need to handle the feature
+    result = response.choices[0].message.content.strip()
+
+    return result
 # endregion
 
 # region UI
@@ -66,19 +142,14 @@ if st.session_state.uploaded_file is not None:
             )
 
             if selected_sheet is not None:
-                st.markdown(f"Displayed data from sheet: ```{selected_sheet}```")
-                df = pd.read_excel(excel_file, sheet_name=selected_sheet)
-                st.dataframe(df)
+                display_dataframe(selected_sheet_name=selected_sheet)
             # endregion
         else:
             # region Handle Single sheet upload
             selected_sheet = sheet_names[0]
-            st.markdown(f"Displayed data from sheet: ```{selected_sheet}```")
-            df = pd.read_excel(excel_file, sheet_name=selected_sheet)
-            st.dataframe(df)
+            display_dataframe(selected_sheet_name=selected_sheet)
             # endregion
     elif file_type == "text/csv":
-        df = pd.read_csv(uploaded_file)
-        st.dataframe(df)
+        display_dataframe(uploaded_file=uploaded_file, is_excel=False)
 
 # endregion
